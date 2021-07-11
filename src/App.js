ed29c74e42c87ddf911d344903ea66f029156193
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-11 16:44:35 +0530

    completed project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import "./App.css";
+import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return null;
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        console.log(res.data);
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+    //to prevent race condition if multiple requests we will cancel prev req everytime we make new req
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />;
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
